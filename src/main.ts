c91648ec945abb313da092f9c848c80ab9057b1c
Joan Llenas <joan.llenas.maso@gmail.com>
2024-03-24 22:48:03 +0100

    renaming

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import './style.css';
 
 import * as Lem from './lem/program';
-import * as Sandbox from './sandbox';
-import * as Element from './element';
+import * as Counter from './counter-basic';
+import * as CouterWithEffects from './counter-with-effects';
 
-Lem.sandbox(Sandbox.init, Sandbox.update, Sandbox.view).run(
+Lem.sandbox(Counter.init, Counter.update, Counter.view).run(
   document.getElementById('app-sandbox')!
 );
 
-Lem.element(Element.init, Element.update, Element.effects, Element.view).run(
-  document.getElementById('app-element')!
-);
+Lem.element(
+  CouterWithEffects.init,
+  CouterWithEffects.update,
+  CouterWithEffects.effects,
+  CouterWithEffects.view
+).run(document.getElementById('app-element')!);
