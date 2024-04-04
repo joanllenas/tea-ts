62661468bc9571660112f8bab0a148ade5848611
Joan Llenas <joan.llenas.maso@gmail.com>
2024-04-04 20:56:08 +0200

    scheduler design

--- src/tea/program.ts
+++ src/tea/program.ts
@@ -71,6 +71,16 @@ export function simple<Model, Msg>(
   };
 }
 
+// ----------------------------
+//
+// ADVANCED
+//
+// ----------------------------
+
+type Scheduler<Msg> = {
+  queue: (msg: Msg) => void;
+};
+
 export function advanced<Model, Msg, Eff extends Effect.Eff<string>, Flags>(
   init: (flags: Flags) => [Model, Eff],
   update: (msg: Msg, model: Model) => [Model, Eff],
@@ -84,87 +94,111 @@ export function advanced<Model, Msg, Eff extends Effect.Eff<string>, Flags>(
         let vnode = patch(node, h('div', 'init'));
         const runningSubs = new Map<string, () => void>();
         const runningEffects = new Map<string, () => void>();
+        let [model, effect] = init(flags);
+        const scheduler: Scheduler<Msg> = initScheduler();
+        let raf = render();
 
-        const loop = ([model, effect]: [Model, Eff]) => {
-          // VDom
-          const newNode = html2vnode(view(model), model);
-          vnode = patch(vnode, newNode);
-
-          // Effects
-          // TODO: handle batched effects
-          const effectFn = effects(effect);
-          const done = (msg: Msg) => {
-            runningEffects.delete(effect.name);
-            loop(update(msg, model));
-          };
-          if (runningEffects.has(effect.name)) {
-            const disposeFn = runningEffects.get(effect.name)!;
-            disposeFn();
-            runningEffects.delete(effect.name);
-          }
-          runningEffects.set(effect.name, effectFn(done).dispose);
-
-          // Subs
-          const newSubs = subscriptions(model);
-          // TODO: this is naive, same key could have a new sub
-          // TODO: handle batched subs
-          // Kill and remove (from old) the ones in old but not in new
-          for (let [key, dispose] of runningSubs) {
-            if (newSubs[key] === undefined) {
-              dispose();
-              // It's safe to delete items from a Map while iterating over it
-              runningSubs.delete(key);
-            }
-          }
-          // Initialize and add (to old) the ones in new but not in old
-          for (let key in newSubs) {
-            if (!runningSubs.has(key)) {
-              const send = (msg: Msg) => {
-                loop(update(msg, model));
-              };
-              const initSubFn = newSubs[key];
-              runningSubs.set(key, initSubFn(send).dispose);
-            }
-          }
-        };
-
-        function html2vnode(html: Html.Html<Msg>, model: Model): VNode {
-          if (html.type === 'Text') {
-            return h('span', html.text);
-          }
-
-          let nodeData: VNodeData = {
-            on: {},
-            attrs: {},
-            children: [],
-          };
-
-          // Attrs
-          html.attributes.forEach((cur) => {
-            if (cur.name === '') {
-              return null;
-            }
-            if (Html.isEvent(cur)) {
-              nodeData.on![cur.name] = () => {
-                loop(update(cur.msg, model));
-              };
-            } else if (Html.isAttr(cur)) {
-              nodeData.attrs![cur.name] = cur.value;
-            }
+        function render() {
+          return window.requestAnimationFrame(() => {
+            const newNode = html2vnode(view(model), model, scheduler);
+            vnode = patch(vnode, newNode);
           });
-
-          // Children
-          const children = html.children.map((cur) => {
-            return html2vnode(cur, model);
-          });
-
-          return h(html.tag, nodeData, children);
         }
 
-        loop(init(flags));
+        function initScheduler() {
+          return {
+            queue: (msg: Msg) => {
+              window.cancelAnimationFrame(raf);
+              [model, effect] = update(msg, model);
+
+              // ----------------------------
+              // Effects
+              // ----------------------------
+
+              // TODO: handle batched effects
+              const effectFn = effects(effect);
+              const done = (msg: Msg) => {
+                runningEffects.delete(effect.name);
+                scheduler.queue(msg);
+              };
+              if (runningEffects.has(effect.name)) {
+                const disposeFn = runningEffects.get(effect.name)!;
+                disposeFn();
+                runningEffects.delete(effect.name);
+              }
+              runningEffects.set(effect.name, effectFn(done).dispose);
+
+              // ----------------------------
+              // Subs
+              // ----------------------------
+              const newSubs = subscriptions(model);
+              // TODO: this is naive, same key could have a new sub
+              // TODO: handle batched subs
+              // Kill and remove (from old) the ones in old but not in new
+              for (let [key, dispose] of runningSubs) {
+                if (newSubs[key] === undefined) {
+                  dispose();
+                  runningSubs.delete(key);
+                }
+              }
+              // Initialize and add (to old) the ones in new but not in old
+              for (let key in newSubs) {
+                if (!runningSubs.has(key)) {
+                  const send = (msg: Msg) => {
+                    scheduler.queue(msg);
+                  };
+                  const initSubFn = newSubs[key];
+                  runningSubs.set(key, initSubFn(send).dispose);
+                }
+              }
+
+              // ----------------------------
+              // Render VDom
+              // ----------------------------
+              raf = render();
+            },
+          };
+        }
       } else {
         throw new Error('Could not mount app, node is not an Html Element');
       }
     },
   };
+}
+
+function html2vnode<Model, Msg>(
+  html: Html.Html<Msg>,
+  model: Model,
+  scheduler: Scheduler<Msg>,
+): VNode {
+  if (html.type === 'Text') {
+    return h('span', html.text);
+  }
+
+  let nodeData: VNodeData = {
+    on: {},
+    attrs: {},
+    children: [],
+  };
+
+  // Attrs
+  html.attributes.forEach((cur) => {
+    if (cur.name === '') {
+      return null;
+    }
+    if (Html.isEvent(cur)) {
+      nodeData.on![cur.name] = () => {
+        scheduler.queue(cur.msg);
+      };
+    } else if (Html.isAttr(cur)) {
+      nodeData.attrs![cur.name] = cur.value;
+    }
+  });
+
+  // Children
+  const children = html.children.map((cur) => {
+    return html2vnode(cur, model, scheduler);
+  });
+
+  return h(html.tag, nodeData, children);
 }
